b66fb6748202667f27bdadfa778acf12453977fe
Joel Schumacher <joelschum@gmail.com>
2019-11-23 17:15:20 +0100

    Add missing MarkerSpawner script

---
+++ godot/MarkerSpawner.gd
@@ -0,0 +1,25 @@
+extends Node
+
+const colors = {
+    "r": Color(1, 0, 0),
+    "g": Color(0, 1, 0),
+    "b": Color(0, 0, 1),
+    "y": Color(1, 1, 0),
+    "p": Color(1, 0, 1),
+}
+
+var markerScene = preload("res://Marker.tscn")
+
+func mark(position : Vector3, color):
+    if color is String:
+        if color in colors:
+            color = colors[color]
+        else:
+            color = Color(color)
+    var marker = markerScene.instance()
+    marker.set_translation(position)
+    var mat = marker.get_surface_material(0).duplicate()
+    mat.albedo_color = color
+    mat.albedo_color.a = 0.4
+    marker.set_surface_material(0, mat)
+    add_child(marker)
